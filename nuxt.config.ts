ee498109d504c0b7e45c10af5aa515ee920516f4
nekonote <User2@nekonote.local>
2023-08-08 15:02:18 +0900

    nuxtConfigの修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,9 +17,9 @@ export default defineNuxtConfig({
       openaiApiKey: "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",
     },
     private: {
-      CHANNEL_ID:"2000270463",
-      channelSecret:"d4219fe02fc63aceb1f2098b39f23482",
-      channelAccessToken:"H3ckSK9bG9S36XS4YxwZvomvrBVs9e5ulD1QaEqibjN1LafkEoGRsGF0yqgtfEbQUxsfETOMO9y/BsAynXLioGh9BUen90i5gP1A8rU+mf1QixNj7o0IIU8JsMI2rPqU16+BVkOXAOoelTdKloxKKQdB04t89/1O/w1cDnyilFU="
+      CHANNEL_ID: process.env.CHANNEL_ID,
+      channelSecret: process.env.CHANNEL_SECRET,
+      channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     }
   }
 })
